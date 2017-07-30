3c32387d36425cb88cf274a62123c9599f7f3968
adi <adi@spectory.com>
2017-07-30 19:11:11 +0300

    client

--- client view demo/socket.js
+++ client view demo/socket.js
@@ -1,53 +1,81 @@
-var wsUri = "ws://echo.websocket.org/";
+// var wsUri = "http://localhost:4000/";
+var wsUri = "ws://localhost:4000/socket/websocket/public:5";
 var output;
 
+
+
 function init() {
+
+    var socket = new Phoenix.Socket("ws://localhost:4000/socket", {})
+
+    socket.connect()
+
+    // Now that you are connected, you can join channels with a topic:
+    let channel;
+
+    join(socket, 'public:5');
+
     output = document.getElementById("output");
-    testWebSocket();
+    // testWebSocket();
 }
 
-function testWebSocket() {
-    websocket = new WebSocket(wsUri);
+var join = function (socket, topic) {
+    channel = socket.channel(topic, {})
+    channel.join()
+        .receive("ok", resp => { console.log("Joined successfully", resp) })
+        .receive("error", resp => { console.log("Unable to join", resp) })
 
-    websocket.onopen = function(evt) {
-        onOpen(evt)
-    };
-
-    websocket.onmessage = function(evt) {
-        onMessage(evt)
-    };
-
-    websocket.onerror = function(evt) {
-        onError(evt)
-    };
+    channel.on("new_msg", payload => {
+        debugger
+        onMessage(payload.body);
+        // let messageItem = document.createElement("li");
+        // messageItem.innerText = `[${Date()}] ${payload.body}`
+        // messagesContainer.appendChild(messageItem)
+    })
 }
 
-function onOpen(evt) {
-    writeToScreen("CONNECTED");
+// function testWebSocket() {
+//     websocket = new WebSocket(wsUri);
+//     debugger
+//     websocket.onopen = function(evt) {
+//         onOpen(evt)
+//     };
+
+//     websocket.onmessage = function(evt) {
+//         onMessage(evt)
+//     };
+
+//     websocket.onerror = function(evt) {
+//         onError(evt)
+//     };
+// }
+
+// function onOpen(evt) {
+//     writeToScreen("CONNECTED");
+// }
+
+function onMessage(msg) {
+    writeToScreen("RECIEVED: " + msg);
 }
 
-function onMessage(evt) {
-    writeToScreen("RECIEVED: " + evt.data);
-}
-
-function onError(evt) {
-    writeToScreen("ERROR:");
-    writeToScreen(evt.data);
-}
-function onClose(evt) {
-    writeToScreen("DISCONNECTED");
- }
+// function onError(evt) {
+//     writeToScreen("ERROR:");
+//     writeToScreen(evt.data);
+// }
+// function onClose(evt) {
+//     writeToScreen("DISCONNECTED");
+//  }
 
 function doSend() {
   var message = document.getElementById("comment").value
     writeToScreen("SENDING: " + message);
-    websocket.send(message);
+    channel.push("new_msg", { body: message});
     document.getElementById("comment").value = ''
 }
 
-function closeSocket() {
-    websocket.close();
-}
+// function closeSocket() {
+//     websocket.close();
+// }
 
 function writeToScreen(message) {
     var pre = document.createElement("p");
