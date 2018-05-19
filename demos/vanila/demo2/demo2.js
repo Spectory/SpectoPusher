e9b0412ff928f774f7a6331721b1cb5875a76e37
Guy Yogev <guy@spectory.com>
2018-05-19 15:18:18 +0300

    Change channel handle_in to brodcast only to other users, and not to self. Removed handle out interceptor.

--- demos/vanila/demo2/demo2.js
+++ demos/vanila/demo2/demo2.js
@@ -4,6 +4,9 @@
  */
 
 $(document).ready(() => {
+  const userId = Math.floor(Math.random() * 10000);
+  $(`#userId`).html(`I'm user  ${userId}`);
+
   // Create a SpectoPusher instance.
   const params = {
     url: 'ws://localhost:4000/socket',
@@ -29,7 +32,7 @@ $(document).ready(() => {
   };
 
   const addMsg = (channelId, msg) => $(`#${channelId}_msgs`).append(`<li class='new-msg'>${msg}</li>`);
-  
+
   const callbacksFor = (channelId) => {
     return {
       onJoinSucc: (res) => { changeChannelStatus(channelId, true); },
@@ -45,10 +48,10 @@ $(document).ready(() => {
   const topic2 = 'public:2';
   SP.join(topic1, callbacksFor('channel_1'));
   SP.join(topic2, callbacksFor('channel_2'));
-  
+
   // Send messages at intervals.
   sendMsg = (topic) => {
-    SP.send(topic, `Look! a random number: ${Math.random()}`);
+    SP.send(topic, `${userId} Says: Look! a random number: ${Math.random()}`);
   }
 
   setInterval(sendMsg, 3000, topic1);
