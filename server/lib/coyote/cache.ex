a10aadbb8b5772105e6597b2c0dcd115ebf33af3
Guy Yogev <guy@spectory.com>
2018-06-13 15:04:00 +0300

    print cache to list

--- server/lib/coyote/cache.ex
+++ server/lib/coyote/cache.ex
@@ -1,4 +1,19 @@
 defmodule Coyote.Cache do
+  @moduledoc """
+  Provides cache object.
+  Cache can be persisted by saving to file.
+
+  ## Examples
+      iex> Coyote.Cache.start_link(:my_cache, true)
+      {:ok, #PID<0.276.0>}
+      iex> Coyote.Cache.insert(:my_cache, {"some_key": "some_value"})
+      iex> Coyote.Cache.insert(:my_cache, {"some_key", "some_value"})
+      :ok
+      iex> Coyote.Cache.lookup(:my_cache, "some_key")
+      "some_value"
+      iex> Coyote.Cache.to_list(:my_cache, "some_key")
+      [{"some_key", "some_value"}]
+  """
   use GenServer
 
   @dets_storage_folder "dets"
@@ -25,6 +40,10 @@ defmodule Coyote.Cache do
 
   def from_file(pid) do
     GenServer.call(pid, {:from_file})
+  end
+
+  def to_list(pid) do
+    GenServer.call(pid, {:to_list})
   end
 
   @impl true
@@ -68,6 +87,11 @@ defmodule Coyote.Cache do
     {:reply, :ok, cache}
   end
 
+  @impl true
+  def handle_call({:to_list}, _from, cache) do
+    {:reply, :ets.tab2list(cache), cache}
+  end
+
   defp save_to_file(cache) do
     {:ok, table} = :dets.open_file(file_name(cache), [type: :set])
     :ets.to_dets(cache, table)
@@ -76,7 +100,6 @@ defmodule Coyote.Cache do
 
   defp load_from_file(cache) do
     {:ok, table} = :dets.open_file(file_name(cache), [type: :set])
-    IO.inspect cache
     cache = :dets.to_ets(table, cache)
     :dets.close(table)
     cache
