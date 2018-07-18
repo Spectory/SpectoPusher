676624c10e9d82af41921665eb7b20d425d8d151
Guy Yogev <guy@spectory.com>
2018-07-18 23:02:25 +0300

    show topics at admin page

--- lib/coyote_web/controllers/admin_controller.ex
+++ lib/coyote_web/controllers/admin_controller.ex
@@ -1,7 +1,25 @@
 defmodule CoyoteWeb.AdminController do
   use CoyoteWeb, :controller
+  use Coyote.Constants
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render_index(conn)
+  end
+
+  def create_topic(conn, %{"topic" => %{"name" => name}}) do
+    create_topic(name)
+    redirect conn, to: "/admin"
+  end
+
+  defp create_topic(name) do
+    Coyote.Cache.insert(@user_channel_cache, {name, 0})
+    Coyote.Cache.to_file(@user_channel_cache)
+  end
+
+  defp render_index(conn) do
+    topics = @user_channel_cache
+      |> Coyote.Cache.to_list
+      |> Enum.map(&(Tuple.to_list &1))
+    render conn, "index.html", topics: topics
   end
 end
