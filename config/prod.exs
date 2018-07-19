4aea17b654656d82c809a56af855cebde111dad5
Guy Yogev <guy@spectory.com>
2018-07-19 21:51:41 +0300

    heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :coyote, CoyoteWeb.Endpoint,
 	load_from_system_env: true,
-	url: [scheme: "https", host: {:system, "HOST"}, {:system, "PORT"}],
+  url: [scheme: "https", host: {:system, "HOST"}, port: 443],
 	cache_static_manifest: "priv/static/cache_manifest.json",
-	secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+	secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
 	force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
