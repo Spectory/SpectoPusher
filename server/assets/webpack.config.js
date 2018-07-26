1c4607062f53b6b2c281c11cecd049f2ea270603
guy yogev <guyogev@gmail.com>
2018-07-27 00:49:19 +0300

    Webpack (#17)
    login page style.
    deps cleanup.

--- server/assets/webpack.config.js
+++ server/assets/webpack.config.js
@@ -1,18 +1,35 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: "./js/app.js",
   output: {
-    path: "../priv/static/js",
-    filename: "app.js"
+    path: "../priv/static",
+    filename: "/js/app.js"
   },
   module: {
-    loaders: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      loader: "babel",
-      include: __dirname,
-      query: {
-        presets: ["es2015", "stage-0"]
-      }
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        include: __dirname,
+        query: {
+          presets: ["es2015", "stage-0"]
+        }
+      },
+      {
+        test: /\.(s)css$/,
+        loader: ExtractTextPlugin.extract(
+          "css-loader!sass-loader",
+        )
+      },
+    ],
   },
+  plugins: [
+    new ExtractTextPlugin('/css/app.css')
+    //if you want to pass in options, you can do so:
+    //new ExtractTextPlugin({
+    //  filename: 'style.css'
+    //})
+  ]
 };
